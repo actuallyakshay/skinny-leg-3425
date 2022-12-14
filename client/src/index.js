c2e887fbd6423925ace7d2bf86d9c2f589455a32
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2022-12-14 10:52:18 +0530

    add_work

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,17 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ChakraProvider>
-    <App />
-  </ChakraProvider>
+  <React.StrictMode>
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+     </ChakraProvider>
+  </BrowserRouter>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
