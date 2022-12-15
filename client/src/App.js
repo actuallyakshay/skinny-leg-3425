09209ab433fc80dd3278c8d84097314b84008879
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2022-12-15 18:14:53 +0530

    add

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import { UFooter } from "./Component/UFooter";
+import { Navbar } from "./Component/Unavbar/Navbar";
 import {LabTest} from "./Pages/LabTest"
 
 function App() {
   return (
     <>
+      <Navbar/>
       <LabTest/>
+      <UFooter/>
     </>
   );
 }
