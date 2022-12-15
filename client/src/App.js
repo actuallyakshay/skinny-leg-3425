891ce3c1ac156bc99a70ef143aac73ed5f3dd911
Ujjwal singh <ujjwalsingh13feb@gmail.com>
2022-12-16 02:21:39 +0530

    conflict resolve

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import { Navbar } from "./Component/Navbar/Navbar";
 import { Router } from "./Component/Router/Router";
-
+import {LabTest} from "./Pages/LabTest";
 function App() {
   return (
     <>
       <Navbar />
       <Router />
+      <LabTest/>
     </>
   );
 }
