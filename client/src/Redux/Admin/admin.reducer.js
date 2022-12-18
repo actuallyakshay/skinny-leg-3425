89a971b6d584711cc933faf5c30132ea67e4cc73
actuallyakshay <akshay.rajput1197@gmail.com>
2022-12-19 04:11:41 +0530

    Added cart

--- client/src/Redux/Admin/admin.reducer.js
+++ client/src/Redux/Admin/admin.reducer.js
@@ -7,6 +7,7 @@ import {
 
 let token = localStorage.getItem("token") || "";
 let role = localStorage.getItem("role") || "";
+let userToken = localStorage.gertItem("userToken") || "";
 
 const iState = {
   token: token,
@@ -15,6 +16,9 @@ const iState = {
   authError: false,
   role: role,
   name: "",
+  phoneNumber: "",
+  userToken: userToken,
+  userAuth: !!userToken,
 };
 
 export const authReducer = (state = iState, { type, payload }) => {
