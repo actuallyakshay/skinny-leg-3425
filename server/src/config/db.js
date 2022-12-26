33ba061d4b4c66be098e70d53266c7a531114003
Akshay Rajput <107462720+actuallyakshay@users.noreply.github.com>
2022-12-26 14:59:48 +0530

    Delete server directory

--- server/src/config/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = connect = async () => {
-  return await mongoose.connect(
-    `mongodb+srv://actuallyakshay:akshay@cluster0.eifrxaw.mongodb.net/Pharmaeasy?retryWrites=true&w=majority`
-  );
-};
